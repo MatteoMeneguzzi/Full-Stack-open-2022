d1803e1157d46e7a7e437a7968d847d986f2e778
Matteo Meneguzzi <matteo.meneguzzi@temera.it>
2022-06-09 16:51:16 +0200

    exercise one completed

---
+++ part1/exercise-one/src/components/Content.js
@@ -0,0 +1,17 @@
+const Content = (props) => {
+  return (
+    <>
+      <p>
+        {props.parts.part1} {props.exercises.exercises1}
+      </p>
+      <p>
+        {props.parts.part2} {props.exercises.exercises2}
+      </p>
+      <p>
+        {props.parts.part3} {props.exercises.exercises3}
+      </p>
+    </>
+  );
+};
+
+export default Content;
