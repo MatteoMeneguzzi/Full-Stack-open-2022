4a22fdf4ac81b47f2266ec607b2ca24b2dea0c24
Matteo Meneguzzi <matteo.meneguzzi@temera.it>
2022-06-09 17:32:26 +0200

    exercise 1.3 completed

--- part1/exercise/src/components/Content.js
+++ part1/exercise/src/components/Content.js
@@ -3,9 +3,18 @@ import Part from "./Part";
 const Content = (props) => {
   return (
     <>
-      <Part part={props.parts.part1} exercise={props.exercises.exercises1} />
-      <Part part={props.parts.part2} exercise={props.exercises.exercises2} />
-      <Part part={props.parts.part3} exercise={props.exercises.exercises3} />
+      <Part
+        part={props.parts.part1.name}
+        exercise={props.parts.part1.exercises}
+      />
+      <Part
+        part={props.parts.part2.name}
+        exercise={props.parts.part2.exercises}
+      />
+      <Part
+        part={props.parts.part3.name}
+        exercise={props.parts.part3.exercises}
+      />
     </>
   );
 };
