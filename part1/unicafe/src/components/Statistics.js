70df6fd8ed7fc26556bbeccbff70641a0e64cde6
Matteo Meneguzzi <matteo.meneguzzi@temera.it>
2022-06-10 12:26:55 +0200

    exercise 1.7 completed

--- part1/unicafe/src/components/Statistics.js
+++ part1/unicafe/src/components/Statistics.js
@@ -1,5 +1,16 @@
 const Statistics = (props) => {
   const { good, neutral, bad } = props.statistics;
+
+  const total = good + neutral + bad;
+
+  const top = 1;
+  const medium = 0;
+  const low = -1;
+
+  const average = (good * top + neutral * medium + bad * low) / total;
+
+  const positive = (good * 100) / total;
+
   return (
     <>
       <h2>statistics</h2>
@@ -7,6 +18,9 @@ const Statistics = (props) => {
       <div>good {good}</div>
       <div>neutral {neutral}</div>
       <div>bad {bad}</div>
+      <div>all {total}</div>
+      <div>average {average ? average : 0}</div>
+      <div>positive {positive ? positive : 0} %</div>
     </>
   );
 };
