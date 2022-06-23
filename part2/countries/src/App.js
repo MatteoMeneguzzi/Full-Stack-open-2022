ca7b8588290013781f5f4e97f5f022425b40fb17
Matteo Meneguzzi <matteo.meneguzzi@temera.it>
2022-06-23 15:31:11 +0200

    changes

--- part2/countries/src/App.js
+++ part2/countries/src/App.js
@@ -1,116 +1,115 @@
-import { useState, useEffect } from "react";
 import axios from "axios";
+import { useState, useEffect } from "react";
 
-const App = () => {
-  const [query, setQuery] = useState("");
-  const [countries, setCountries] = useState([]);
-  const [temperature, setTemperature] = useState();
-  const [wind, setWind] = useState();
+const Weather = ({ weather, city }) => {
+  if (weather === null) return null;
 
-  const handleQuery = (e) => {
-    const { value } = e.target;
+  const icon = `http://openweathermap.org/img/wn/${weather.weather[0].icon}@2x.png`;
 
-    setQuery(value);
-  };
+  return (
+    <div>
+      <h3>Weather in {city}</h3>
 
-  let filteredData = countries.filter((country) =>
-    country.name.common.toLowerCase().includes(query.toLowerCase())
-  );
+      <div>temperature {weather.main.temp} Celcius</div>
 
-  const showInfo = (item) => {
-    const value = item.target.parentElement.childNodes[0].data;
-
-    setQuery(value);
-  };
-
-  console.log(filteredData);
-  let countriesList = filteredData.map((item, index) => (
-    <>
-      <div key={index}>
-        {item.name.common}
-        <button onClick={showInfo}>show</button>
-      </div>
-    </>
-  ));
-
-  const api_key = process.env.REACT_APP_API_KEY;
-  console.log(api_key);
-  console.log(
-    axios.get(
-      `https://api.openweathermap.org/data/2.5/weather?lat={lat}&lon={lon}&appid=${api_key}`
-    )
-  );
-
-  let singleCountry = filteredData.map((item, index) => {
-    let languages = [];
-    for (const key in item.languages) {
-      if (Object.hasOwnProperty.call(item.languages, key)) {
-        languages.push(item.languages[key]);
-      }
-    }
-
-    const lat = item.capitalInfo.latlng[0].toFixed(2);
-
-    const lng = item.capitalInfo.latlng[1].toFixed(2);
-
-    function fToC(fahrenheit) {
-      var fTemp = fahrenheit;
-      var fToCel = ((fTemp - 32) * 5) / 9;
-      return fToCel;
-    }
-
-    axios
-      .get(
-        `https://api.openweathermap.org/data/3.0/onecall?lat=${lat}.44&lon=${lng}&exclude=hourly,daily&appid=${api_key}`
-      )
-      .then((res) => {
-        let newTemperature = fToC(res.data.main.temp);
-        setTemperature(newTemperature);
-
-        let newWind = res.data.wind.speed;
-        setWind(newWind);
-      });
-
-    return (
       <div>
-        <h2>{item.name.common}</h2>
-        <div>capital {item.capital}</div>
-        <div>area {item.area}</div>
-
-        <h3>languages:</h3>
-
-        <ul>
-          {languages.map((language) => (
-            <li>{language}</li>
-          ))}
-        </ul>
-
-        <img src={item.flags.png} />
-        <h2>Weather in {item.capital}</h2>
-        <div>temperature {temperature} Celsius</div>
-        <img src={item.flags.png} />
-        <div>wind {wind} m/s</div>
+        <img src={icon} alt={`icon for ${weather.weather[0].description}`} />
       </div>
-    );
-  });
+
+      <div>wind {weather.wind.speed} m/s</div>
+    </div>
+  );
+};
+
+const Country = ({ country }) => {
+  const [weather, setWeather] = useState(null);
+  const languages = Object.values(country.languages);
+  const capital = country.capital[0];
 
   useEffect(() => {
-    axios.get("https://restcountries.com/v3.1/all").then((res) => {
-      setCountries(res.data);
+    const key = process.env.REACT_APP_API_KEY;
+    const url = `http://api.openweathermap.org/data/2.5/weather?q=${capital}&appid=${key}&units=metric`;
+    axios.get(url).then(({ data }) => {
+      // setWeather(data);
+      console.log(data);
     });
   }, []);
 
   return (
     <div>
-      find countries <input value={query} onChange={handleQuery} />
-      {countriesList.length > 10 ? (
-        <div>Too many matches, specify another filter</div>
-      ) : countriesList.length === 1 ? (
-        <>{singleCountry.pop()}</>
-      ) : (
-        <>{countriesList}</>
-      )}
+      <h2>{country.name.common}</h2>
+      <div>capital {capital} </div>
+      <div>area {country.area} </div>
+
+      <h4>languages:</h4>
+      <ul>
+        {languages.map((language) => (
+          <li key={language}>{language}</li>
+        ))}
+      </ul>
+
+      <img
+        src={country.flags.png}
+        alt={`Flag of ${country.name.common}`}
+        width={150}
+      />
+
+      <Weather weather={weather} city={capital} />
     </div>
+  );
+};
+
+const Contrylist = ({ countries, setFilter }) => {
+  console.log(countries);
+  if (countries.length > 10) {
+    return <div>Too many countries, specify some other fliter</div>;
+  }
+
+  if (countries.length === 0) {
+    return <div>No matches, specify some other fliter</div>;
+  }
+
+  if (countries.length > 1) {
+    return (
+      <div>
+        {countries.map(({ name }) => (
+          <div key={name.common}>
+            {name.common}
+            <button onClick={() => setFilter(name.common)}>show</button>
+          </div>
+        ))}
+      </div>
+    );
+  }
+
+  return <Country country={countries[0]} />;
+};
+
+const App = () => {
+  const [countries, setCountries] = useState([]);
+  const [filter, setFilter] = useState("");
+
+  useEffect(() => {
+    axios.get("https://restcountries.com/v3.1/all").then(({ data }) => {
+      setCountries(data);
+    });
+  }, []);
+
+  const filtered = countries.filter((c) =>
+    c.name.common.toLowerCase().includes(filter.toLowerCase())
+  );
+
+  return (
+    <>
+      <div>
+        find countries
+        <input
+          value={filter}
+          onChange={({ target }) => setFilter(target.value)}
+        />
+      </div>
+      <Contrylist countries={filtered} setFilter={setFilter} />
+    </>
   );
 };
 
